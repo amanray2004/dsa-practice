7681f170f260d69a28a0e9166b8d777b74084f63
amanray2004 <119524133+amanray2004@users.noreply.github.com>
2023-09-12 14:07:25 +0530

    y

---
+++ longest_consecutive_sequence_p128/Solution.java
@@ -0,0 +1,33 @@
+//brute approach
+
+package longest_consecutive_sequence_p128;
+
+
+//import java.util.Arrays;
+    
+
+
+public class Solution {
+    static boolean ispresent(int arr[],int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target)
+                return true;
+        }
+        return false;
+    }
+    public int longestConsecutive(int[] nums) {
+        int largeststreak=0;
+        for (int i=0;i<nums.length;i++){
+            int currentnumber=nums[i];
+            int currentstreak=1;
+            while(ispresent(nums,currentnumber+1)){
+                currentnumber+=1;
+                currentstreak+=1;
+            }
+            if(currentstreak>largeststreak){
+                largeststreak=currentstreak;
+            }
+        }
+        return largeststreak;
+    }
+}
