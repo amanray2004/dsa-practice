826a9b2b5f7cd00b7a58ed78e7cb8c557a3c8852
amanray2004 <119524133+amanray2004@users.noreply.github.com>
2023-09-17 21:55:49 +0530

    Update Solution.java

--- longest_consecutive_sequence_p128/consecutive_one's/Solution.java
+++ longest_consecutive_sequence_p128/consecutive_one's/Solution.java
@@ -1,29 +1,37 @@
-package longest_consecutive_sequence_p128.consecutive_one's;
 import java.util.*;
+
 public class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        HashMap<Integer, Integer> a
-            = new HashMap<>();
-        int b=0;
-         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                a.put(b,i);
-            }
-
-         }
-        int max=0;
-        for(int i=0;i<b;i++){
-            if(a.get(i+1)-a.get(i)>max){
-                max=a.get(i+1)-a.get(i);
+        HashMap<Integer, Integer> a = new HashMap<>();
+        int b = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                a.put(b, i);
+                b = 0; // Reset consecutive count of zeros
+            } else {
+                b++; // Increment consecutive count of zeros
             }
         }
+
+        // Handle the case where there are no zeros
+        if (a.isEmpty()) {
+            return nums.length;
+        }
+
+        int max = 0;
+        for (int i = 0; i < b; i++) {
+            if (a.containsKey(i + 1)) {
+                int consecutiveOnes = a.get(i + 1) - a.get(i) - 1;
+                max = Math.max(max, consecutiveOnes);
+            }
+        }
+
         return max;
     }
 
     public static void main(String[] args) {
-        int[] a={1,1,0,1,1,1};
-        Solution s=new Solution();
-        System.out.println("hello");
+        int[] a = { 1, 1, 0, 1, 1, 1 };
+        Solution s = new Solution();
         System.out.println(s.findMaxConsecutiveOnes(a));
     }
 }
